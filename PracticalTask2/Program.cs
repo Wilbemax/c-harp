d53e38c78ba0cdcfd4fd57df85f43b1682c68617
Wilbemax <max22042007@yandex.ru>
2024-09-19 21:30:52 +0300

    fix report

--- PracticalTask2/Program.cs
+++ PracticalTask2/Program.cs
@@ -27,38 +27,38 @@ class Hallo
 
         //Задание 2  Наименьшую цифру в натуральном двухзначном числе
 
-        int number;
-        int smallestDigit;
+        // int number;
+        // int smallestDigit;
 
-        Console.Write("Введите двузначное натуральное число: ");
-        number = int.Parse(Console.ReadLine());
-
-
-        if (number < 10 || number > 99)
-        {
-            Console.WriteLine("Число должно быть двузначным.");
-            return;
-        }
+        // Console.Write("Введите двузначное натуральное число: ");
+        // number = int.Parse(Console.ReadLine());
 
 
-        // int firstDigit = number / 10;
-        // int secondDigit = number % 10;
+        // if (number < 10 || number > 99)
+        // {
+        //     Console.WriteLine("Число должно быть двузначным.");
+        //     return;
+        // }
 
 
-        // smallestDigit = Math.Min(firstDigit, secondDigit);
+        // // int firstDigit = number / 10;
+        // // int secondDigit = number % 10;
+
+
+        // // smallestDigit = Math.Min(firstDigit, secondDigit);
 
 
         
-        // or
+        // // or
 
-        if (number % 10 >= number / 10) {
-            smallestDigit = number % 10 ;
-        } else {
-            smallestDigit = number / 10;
-        }
+        // if (number % 10 >= number / 10) {
+        //     smallestDigit = number % 10 ;
+        // } else {
+        //     smallestDigit = number / 10;
+        // }
 
 
-        Console.WriteLine("Наименьшая цифра в числе {0} равна {1}", number, smallestDigit);
+        // Console.WriteLine("Наименьшая цифра в числе {0} равна {1}", number, smallestDigit);
 
 
         // Задание 3) Число Х является перевертышем числа Y. Зная значение числа Y, найдите исходное число X.
