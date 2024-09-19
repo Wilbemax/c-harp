170ae2fc335187393105f04fef6d3092936cb5df
Wilbemax <max22042007@yandex.ru>
2024-09-19 13:13:48 +0300

    do practical task 2

--- PracticalTask2/Program.cs
+++ PracticalTask2/Program.cs
@@ -28,24 +28,57 @@ class Hallo
         //Задание 2  Наименьшую цифру в натуральном двухзначном числе
 
         int number;
+        int smallestDigit;
+
         Console.Write("Введите двузначное натуральное число: ");
         number = int.Parse(Console.ReadLine());
 
-            
+
         if (number < 10 || number > 99)
         {
             Console.WriteLine("Число должно быть двузначным.");
             return;
         }
 
-        
-        int firstDigit = number / 10;
-        int secondDigit = number % 10;
+
+        // int firstDigit = number / 10;
+        // int secondDigit = number % 10;
+
+
+        // smallestDigit = Math.Min(firstDigit, secondDigit);
+
 
         
-        int smallestDigit = Math.Min(firstDigit, secondDigit);
+        // or
+
+        if (number % 10 >= number / 10) {
+            smallestDigit = number % 10 ;
+        } else {
+            smallestDigit = number / 10;
+        }
+
 
         Console.WriteLine("Наименьшая цифра в числе {0} равна {1}", number, smallestDigit);
 
+
+        // Задание 3) Число Х является перевертышем числа Y. Зная значение числа Y, найдите исходное число X.
+
+        // int x;
+        // int y;
+
+        // Console.Write("Введите Y:");
+        // y = int.Parse(Console.ReadLine());
+
+        // int first;
+        // int second;
+        // int third;
+
+        // first = y % 10;
+        // y = y / 10;
+        // second = y % 10;
+        // y = y / 10;
+        // third = y % 10;
+
+        // Console.WriteLine("X = {0}{1}{2}", first, second, third);
     }
 }
