930611f20b763b71439445fffab217dd38ce4e85
Wilbemax <max22042007@yandex.ru>
2024-11-07 10:31:57 +0300

    pr 6 \7

--- PracticalTask6/Program.cs
+++ PracticalTask6/Program.cs
@@ -25,13 +25,13 @@ class Program
 
     static void PrintInMatrix(int[,] array)
     {
-        Console.WriteLine("Matrix view");
+        Console.WriteLine("\nMatrix view");
         for (int i = 0; i < array.GetLength(0); i++)
         {
 
             for (int j = 0; j < array.GetLength(1); j++)
             {
-                Console.Write("{0} ", array[i, j]);
+                Console.Write("{0,3} ", array[i, j]);
             }
             Console.WriteLine();
         }
@@ -40,66 +40,71 @@ class Program
 
     static void Main()
     {
-        // побочная диоганаль i + j < n - 1
+        // побочная диоганаль i + j >= n 
 
         int[,] array;
         Input(out array);
         // 1 (5)
 
-        // int sum = 0;
-        // int count = 0;
+        int sum = 0;
+        int count = 0;
 
-        // for (int i = 1; i < array.GetLength(0); i++)
-        // {
-        //     for (int j = 0; j < i; j++)
-        //     {
-        //         sum += array[i, j];
-        //         count++;
-        //     }
-        // }
+        int n = array.GetLength(0);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i + j >= n)
+                {
+                    Console.Write("{0} ", array[i, j]);
+                    sum += array[i, j];
+                    count++;
+                }
+            }
+        }
 
-        // if (count > 0)
-        // {
-        //     double aver = (double)sum / count;
-        //     Console.WriteLine("Среднее значение элементов ниже главной диагонали: " + aver);
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Нет элементов ниже главной диагонали.");
-        // }
+        if (count > 0)
+        {
+            double aver = (double)sum / count;
+            Console.WriteLine("Среднее значение элементов ниже главной диагонали: " + aver);
+        }
+        else
+        {
+            Console.WriteLine("Нет элементов ниже главной диагонали.");
+        }
 
 
         //2 (9)
 
-        int[] res = new int[array.GetLength(1)];
+        // int[] res = new int[array.GetLength(1)];
 
 
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                // Console.Write("{0} ", array[i, j]);
+        // for (int j = 0; j < array.GetLength(1); j++)
+        // {
+        //     for (int i = 0; i < array.GetLength(0); i++)
+        //     {
+        //         // Console.Write("{0} ", array[i, j]);
 
-                if (array[i, j] % 2 != 0)
-                {
-                    res[j] = array[i, j];
-                } 
+        //         if (array[i, j] % 2 != 0)
+        //         {
+        //             res[j] = array[i, j];
+        //         } 
 
             
-            }
-        }
+        //     }
+        // }
+        // // Console.WriteLine();
+
+
+
+        // Console.Write("res:  ");
+        // for (int i = 0; i < res.Length; i++)
+        // {
+            
+        //     Console.Write(" {0} ", res[i]);
+        // }
+
         // Console.WriteLine();
-
-
-
-        Console.Write("res:  ");
-        for (int i = 0; i < res.Length; i++)
-        {
-            
-            Console.Write(" {0} ", res[i]);
-        }
-
-        Console.WriteLine();
 
         PrintInMatrix(array);
     }
---
+++ PracticalTask7/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str;
+
+        Console.Write("Введите строку: ");
+        str = Console.ReadLine();
+
+        string x;
+        string y;
+
+        int countX = 0, countY = 0;
+
+        Console.Write("Введите букву №1 для проверки: ");
+        x = Console.ReadLine();
+
+        Console.Write("Введите букву №2 для проверки: ");
+        y = Console.ReadLine();
+
+        // 1
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == x[0]) countX++;
+            if (str[i] == y[0]) countY++;
+        }
+
+        Console.WriteLine("\nКол-во вхождений выбранных символов");
+        Console.WriteLine("x: {0}, y: {1}", countX, countY);
+
+        // 2
+        if (countX > countY)
+        {
+            Console.WriteLine("Символ №1 '{0}' встречается чаще", x);
+        }
+        else if (countX < countY)
+        {
+            Console.WriteLine("Символ №2 '{0}' встречается чаще", y);
+        }
+        else
+        {
+            Console.WriteLine("Символы '{0}' и '{1}' равны по количеству вхождения", x, y);
+        }
+
+        // 3
+        string res = "";
+
+        for (int i = 0; i < str.Length - 1; i += 2)
+        {
+            if (str[i] == str[i + 1])
+            {
+                res += str[i].ToString() + str[i + 1].ToString();
+            }
+        }
+
+        if (res.Length == 0)
+        {
+            Console.WriteLine("Нет попарных символов");
+        }
+        else
+        {
+            Console.WriteLine("Попарные символы: {0}", res);
+        }
+    }
+}
